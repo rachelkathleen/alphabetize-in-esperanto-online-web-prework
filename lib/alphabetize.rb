0b729e02fa178d04978c0ca51addf96a55f9fcd8
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-25 05:43:20 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
-  # code here
+  arr.sort_by do |string|
+    string.split("").map do |character|
+      ESPERANTO_ALPHABET.index(character)
+    end
+  end
 end
